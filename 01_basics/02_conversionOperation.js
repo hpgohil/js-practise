433e8417157d0fed553a3cd889e352b34ef1dbcb
hpgohil <126784706+hpgohil@users.noreply.github.com>
2024-05-10 18:12:42 +0000

    conversion of datatypes

---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,9 @@
+// let score = "33";
+// let score = null;
+let score = undefined;
+
+
+let valueInNum = Number(score);
+console.log(score, typeof score, valueInNum, typeof valueInNum);
+
+console.log(Boolean(1), Boolean(0), Boolean(null), Boolean(undefined), Boolean(""), Boolean({}));
