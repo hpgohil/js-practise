8ac6608afcfab642e5e225f894112093841f619a
hpgohil <126784706+hpgohil@users.noreply.github.com>
2024-05-10 15:52:53 +0000

    let, const and var explain

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 23;
+let accountEmail = "hari@google.com";
+var accountPassword = "121212";
+// accountCity: "Baroda";
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountState])
