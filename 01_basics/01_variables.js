5ceb10477d5b5650ef7b687886d9c079922f19e3
hpgohil <126784706+hpgohil@users.noreply.github.com>
2024-05-20 11:51:33 +0000

    conversion of data types examples

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 23;
 let accountEmail = "hari@google.com";
 var accountPassword = "121212";
-// accountCity: "Baroda";
+accountCity: "Baroda"; //Error: accountCity is not defined. so reference error
 let accountState;
 
 console.table([accountId, accountEmail, accountPassword, accountState])
--- 01_basics/02_conversionOperation.js
+++ 01_basics/02_conversionOperation.js
@@ -4,6 +4,48 @@ let score = undefined;
 
 
 let valueInNum = Number(score);
-console.log(score, typeof score, valueInNum, typeof valueInNum);
+// console.log(score, typeof score, valueInNum, typeof valueInNum);
 
-console.log(Boolean(1), Boolean(0), Boolean(null), Boolean(undefined), Boolean(""), Boolean({}));
+// console.log(Boolean(1), Boolean(0), Boolean(null), Boolean(undefined), Boolean(""), Boolean({}));
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************************* OPERATIONS ******************************************
+let counter = 50;
+// counter++;
+// console.log(counter);
+// ++counter;
+// console.log(counter);
+
+// const newCounter = counter++;
+// console.log(newCounter);
+// console.log(counter);
+
+let newCounter = ++counter;
+// console.log(newCounter);
+// console.log(counter);
+
+let no = Number(undefined);
+// console.log(no);
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id, anotherId)
+// console.log(id===anotherId)
+
+const fruits = ['mango', 'apple', 'banana']
+
+const myObj = {
+    name: "hari",
+    city: "Vadodara"
+}
+const myFunc = function(){}
+
+console.log(typeof fruits, typeof myObj, typeof myFunc)
