d90d4b2332c5aa7441a6b7e1ef06aeeeeaf62902
hpgohil <126784706+hpgohil@users.noreply.github.com>
2024-05-21 15:12:24 +0000

    Date methods explained

---
+++ 01_basics/05_dates.js
@@ -0,0 +1,30 @@
+let myDate = new Date();
+console.log(typeof myDate);
+console.log("toDateString", myDate.toDateString());
+console.log("toString", myDate.toString());
+console.log("toLocalString", myDate.toLocaleString());
+console.log("toLocalDateString", myDate.toLocaleDateString());
+console.log("JSON", myDate.toJSON());
+console.log("ISO", myDate.toISOString());
+console.log("getMonth", myDate.getMonth())
+console.log("getDay", myDate.getDay());
+console.log("getDate", myDate.getDate())
+console.log("getTime", myDate.getTime());
+
+
+
+
+let specificDate = new Date(2024, 4, 21, 20,30,53)
+// console.log(specificDate);
+// console.log(specificDate.toDateString());
+
+specificDate = new Date("2024-05-21")
+// console.log(specificDate);
+// console.log(specificDate.toDateString());
+// console.log(specificDate.toLocaleDateString());
+// console.log(specificDate.toLocaleString());
+// console.log(specificDate.toString());
+
+
+
+
