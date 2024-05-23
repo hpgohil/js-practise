1802d7f3b200e2a01e827d749579db689c064d0d
hpgohil <126784706+hpgohil@users.noreply.github.com>
2024-05-23 04:55:16 +0000

    Object methods explained

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,30 @@
+const myObj = new Object() //singleton object
+// console.log(myObj);
+
+const tinderUser = {} // non-singleton object
+tinderUser.id = '123abc';
+tinderUser.name = 'Hari';
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+//merge two or more objects
+
+const obj1 = {a:1, b:2}
+const obj2 = {c:1, d:2}
+
+const obj3 = {obj1, obj2}
+// console.log(obj3);
+
+//Object.assing(target, source1, source2)
+const obj4 = Object.assign({}, obj1, obj2)
+// console.log(obj4);
+
+//spread operator
+const obj5 = {...obj1, ...obj2}
+// console.log(obj5);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
