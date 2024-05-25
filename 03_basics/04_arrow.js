7b5b881181727e3535956a7745c3d89b8e6cc8e3
hpgohil <126784706+hpgohil@users.noreply.github.com>
2024-05-25 09:08:36 +0000

    "this" keyword & arrow function explained

---
+++ 03_basics/04_arrow.js
@@ -0,0 +1,44 @@
+// console.log(this);
+
+const myObj = {
+  username: "Hari",
+  age: 30,
+  welcomeMessage: function () {
+    console.log(`Welcome, ${this.username}`);
+    // return `Welcome, ${myObj.username}`;
+  },
+};
+// console.log(myObj);
+// console.log(myObj.welcomeMessage());
+// myObj.username="Ram";
+// console.log(myObj.welcomeMessage());
+myObj.welcomeMessage;
+// myObj.welcomeMessage();
+
+myObj.username = "Ram";
+// myObj.welcomeMessage();
+
+function chai() {
+  console.log(this); // a big object
+}
+// chai();
+
+function one() {
+  const user = "Hari";
+  console.log(this.user); //undefined
+}
+// one();
+
+const two = function () {
+  console.log(this); //a big object
+  const username = "Someone";
+  console.log(this.username); //undefined
+};
+// two();
+
+const three = () => {
+  console.log(this); //{}
+  const username = "Krishna";
+  console.log(this.username); //undefined
+};
+three();
