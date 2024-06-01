09a44fad09bb1f7adf5b940a35cdbaaceb566a14
hpgohil <126784706+hpgohil@users.noreply.github.com>
2024-06-01 11:01:09 +0000

    for in loop explained

---
+++ 05_iterations/04_forin.js
@@ -0,0 +1,19 @@
+//forin loop on object
+const codingLanguages = {
+  js: "Javascript",
+  rb: "Ruby",
+  cpp: "C++",
+  swift: "Swift by apple",
+};
+for (const key in codingLanguages) {
+  //   console.log(key);
+  //   console.log(codingLanguages[key]);
+  //   console.log(`${key} is the shortcut for ${codingLanguages[key]}`);
+}
+
+//forin loop on Array
+const myArr = ["js", "rb", "cpp", "swift"];
+for (const key in myArr) {
+  //   console.log(key);
+  //   console.log(myArr[key]);
+}
