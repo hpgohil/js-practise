c9e439335d53847b643f8fbcd9bd30ec171e3113
hpgohil <126784706+hpgohil@users.noreply.github.com>
2024-06-01 11:00:12 +0000

    if conditions, switch & truthy values explained

---
+++ 04_control_flow/01_if.js
@@ -0,0 +1,17 @@
+const isLoggedIn = true;
+
+if (isLoggedIn) {
+  console.log("User logged in");
+} else {
+  console.log("User didn't log in");
+}
+
+const temp = 42;
+
+if (temp >= 40) {
+  console.log("temp higher than 40");
+} else if (temp <= 40) {
+  console.log("temp less than 40");
+} else {
+  console.log("temp mention");
+}
